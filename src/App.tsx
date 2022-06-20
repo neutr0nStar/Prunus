4b6ddd3d3652389bd383000c756a3eb5fbb9e6b8
neutr0nStar <snebelsare@gmail.com>
2022-06-20 10:24:47 +0530

    initialized vite project and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-4xl min-h-screen text-white bg-blue-600 flex justify-center items-center'>Hello world</div>
+  )
+}
+
+export default App
